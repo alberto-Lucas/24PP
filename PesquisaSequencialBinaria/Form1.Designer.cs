ca59b48eff1875e8df1810135469491436dc850a
Lucas Alberto <lucas.alberto@linx.com>
2024-04-05 10:21:34 -0300

    PesquisaSequencialBinaria

---
+++ PesquisaSequencialBinaria/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace PesquisaSequencialBinaria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSequencial = new System.Windows.Forms.Button();
+            this.btnBinaria = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSequencial
+            // 
+            this.btnSequencial.Location = new System.Drawing.Point(12, 36);
+            this.btnSequencial.Name = "btnSequencial";
+            this.btnSequencial.Size = new System.Drawing.Size(75, 23);
+            this.btnSequencial.TabIndex = 0;
+            this.btnSequencial.Text = "Sequencial";
+            this.btnSequencial.UseVisualStyleBackColor = true;
+            this.btnSequencial.Click += new System.EventHandler(this.btnSequencial_Click);
+            // 
+            // btnBinaria
+            // 
+            this.btnBinaria.Location = new System.Drawing.Point(93, 36);
+            this.btnBinaria.Name = "btnBinaria";
+            this.btnBinaria.Size = new System.Drawing.Size(75, 23);
+            this.btnBinaria.TabIndex = 1;
+            this.btnBinaria.Text = "Binaria";
+            this.btnBinaria.UseVisualStyleBackColor = true;
+            this.btnBinaria.Click += new System.EventHandler(this.btnBinaria_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(464, 36);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(174, 38);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(527, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "10, 20, 30, 40, 50, 60, 70, 80, 90";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResultado.Location = new System.Drawing.Point(12, 65);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResultado.Size = new System.Drawing.Size(527, 220);
+            this.txtResultado.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 297);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnBinaria);
+            this.Controls.Add(this.btnSequencial);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSequencial;
+        private System.Windows.Forms.Button btnBinaria;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtResultado;
+    }
+}
+
---
+++ PesquisaSequencialBinaria/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PesquisaSequencialBinaria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int count;
+
+        int Sequencial(int[] array, int target)
+        {
+            count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                count++;
+
+                txtResultado.Text += 
+                    ($"{array[i]}  = {target} : {(array[i] == target ? "Sim" : "Não")}" ) + Environment.NewLine;
+
+                if (array[i] == target)
+                {
+                    return array[i]; // Retorna o índice onde o elemento foi encontrado
+                }
+                
+            }
+            return -1; // Retorna -1 se o elemento não foi encontrado
+        }
+
+        private void btnSequencial_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = "";
+            int[] array = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+            int target = int.Parse(txtValor.Text);
+            int result = Sequencial(array, target);
+
+            if (result != -1)
+            {
+                txtResultado.Text += "\n" + ($"Executado {count} vez(es)") + Environment.NewLine;
+            }
+            else
+            {
+                txtResultado.Text += "\n" + ($"Executado {count} vez(es)") + Environment.NewLine;
+                txtResultado.Text += "\n" + ($"Elemento {target} não encontrado no array") + Environment.NewLine;
+            }
+        }
+
+        int BinarySearchAlgorithm(int[] array, int target)
+        {
+            count = 0;
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                count++;
+                
+                int mid = left + (right - left) / 2;
+
+                txtResultado.Text +=
+                    ($"{array[mid]}  = {target} : {(array[mid] == target ? "Sim" : "Não")}") + Environment.NewLine;
+
+                if (array[mid] == target)
+                {
+                    return mid; // Retorna o índice onde o elemento foi encontrado
+                }
+                else if (array[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return -1; // Retorna -1 se o elemento não foi encontrado
+        }
+
+        private void btnBinaria_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = "";
+            int[] array = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+            int target = int.Parse(txtValor.Text);
+            int result = BinarySearchAlgorithm(array, target);
+
+            if (result != -1)
+            {
+                txtResultado.Text += "\n" + ($"Executado {count} vez(es)") + Environment.NewLine;
+            }
+            else
+            {
+                txtResultado.Text += "\n" + ($"Executado {count} vez(es)") + Environment.NewLine;
+                txtResultado.Text += "\n" + ($"Elemento {target} não encontrado no array") + Environment.NewLine;
+            }
+        }
+    }
+}
