c33c34ed827180eec6464e904723e30a71ade1c5
alberto-Lucas <lucasalberto.etec@hotmail.com>
2024-04-20 10:24:02 -0300

    AppFila

---
+++ AppFila/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace AppFila
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCapacidade = new System.Windows.Forms.TextBox();
+            this.btnCriar = new System.Windows.Forms.Button();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.lstFila = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Capacidade:";
+            // 
+            // txtCapacidade
+            // 
+            this.txtCapacidade.Location = new System.Drawing.Point(12, 25);
+            this.txtCapacidade.Name = "txtCapacidade";
+            this.txtCapacidade.Size = new System.Drawing.Size(100, 20);
+            this.txtCapacidade.TabIndex = 1;
+            // 
+            // btnCriar
+            // 
+            this.btnCriar.Location = new System.Drawing.Point(118, 23);
+            this.btnCriar.Name = "btnCriar";
+            this.btnCriar.Size = new System.Drawing.Size(75, 23);
+            this.btnCriar.TabIndex = 2;
+            this.btnCriar.Text = "Criar";
+            this.btnCriar.UseVisualStyleBackColor = true;
+            this.btnCriar.Click += new System.EventHandler(this.btnCriar_Click);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(12, 72);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Valor:";
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(118, 70);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(75, 23);
+            this.btnInserir.TabIndex = 5;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(199, 70);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(75, 23);
+            this.btnRemover.TabIndex = 6;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Log:";
+            // 
+            // lstLog
+            // 
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.Location = new System.Drawing.Point(12, 131);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(224, 199);
+            this.lstLog.TabIndex = 8;
+            // 
+            // lstFila
+            // 
+            this.lstFila.FormattingEnabled = true;
+            this.lstFila.Location = new System.Drawing.Point(242, 131);
+            this.lstFila.Name = "lstFila";
+            this.lstFila.Size = new System.Drawing.Size(67, 199);
+            this.lstFila.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(242, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Fila:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 339);
+            this.Controls.Add(this.lstFila);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lstLog);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCriar);
+            this.Controls.Add(this.txtCapacidade);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Fila";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCapacidade;
+        private System.Windows.Forms.Button btnCriar;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstLog;
+        private System.Windows.Forms.ListBox lstFila;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ AppFila/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppFila
+{
+    public partial class Form1 : Form
+    {
+        Queue<int> fila; //Fila de numeros inteiros
+        int capacidade; //Capacidade de elementos da fila
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Método para criação da fila
+        void CriarFila(int elementos)
+        {
+            //Define a quantidad de elementos na fila
+            capacidade = elementos;
+
+            //Criar a fila em memoria
+            fila = new Queue<int>();
+
+            lstLog.Items.Add("Fila criada");
+            lstLog.Items.Add("Capacidade: " + capacidade.ToString());
+        }
+
+        //Função para verificar se a fila está cheia
+        bool FilaCheia()
+        {
+            //Retorna se quantidade de elementos na fila é igual a capacidade
+            return fila.Count == capacidade;
+        }
+
+        //Função para verificar se a fila está vazia
+        bool FilaVazia()
+        {
+            //Verifica se a quantidade de elemesnto na fila é zero
+            return fila.Count == 0;
+        }
+
+        //Metodo para atualizar a fila para o usuario
+        void AtualizarListaFila()
+        {
+            //Limpa a lista
+            lstFila.Items.Clear();
+
+            //Isso aqui não funciona
+            //pois o primeiro elemento não nescessariamente
+            //estara na posição zero
+            //ou seja o primeiro elemento tem sua posição variada
+            /*
+            for(int i = 0; i <= fila.Count; i++)
+            {
+                int elemento = fila[i];
+            }*/
+
+            //Portanto precisamos utilizar
+            //Laço de repetição, para percorrer os elementos
+            //e não a posição deles
+            foreach(var elemento in fila)
+            {
+                lstFila.Items.Add(elemento.ToString());
+            }
+        }
+
+        //Metodo para inserir um valor na fila
+        void Enqueue(int valor)
+        {
+            //Validmos se a fila esta cheia
+            //ou seja se a quantidade de elementos é igual a capacidade
+            if (FilaCheia())
+                lstLog.Items.Add("Fila Cheia!");
+            else
+            {
+                //Inserirmos o valor na Fila]
+                fila.Enqueue(valor);
+
+                //Apresentamos o log
+                lstLog.Items.Add("Enqueue");
+                lstLog.Items.Add("Valor: " + valor.ToString());
+
+                AtualizarListaFila();
+            }
+        }
+
+        //Metodo para remover um valor da fila
+        void Dequeue()
+        {
+            //Validar se a fila esta vazia
+            if (FilaVazia())
+                lstLog.Items.Add("Fila Vazia!");
+            else
+            {
+                //Remover o primeiro elemento da fila
+                //e exibir no log
+                int primeiroElemento = fila.Dequeue();
+                lstLog.Items.Add("Dequeue");
+                lstLog.Items.Add("Valor: " + primeiroElemento.ToString());
+
+                AtualizarListaFila();
+            }
+        }
+
+        private void btnCriar_Click(object sender, EventArgs e)
+        {
+            lstLog.Items.Clear();
+            lstFila.Items.Clear();
+            CriarFila(int.Parse(txtCapacidade.Text));
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            Enqueue(int.Parse(txtValor.Text));
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            Dequeue();
+        }
+    }
+}
